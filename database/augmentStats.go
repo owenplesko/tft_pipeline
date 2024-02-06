a2735bfb276c0a2e76a96195e3c6f4b920029ee6
Owen Plesko <owenplesko@gmail.com>
2024-02-06 11:55:45 -0500

    fixed bug with augment stats query

--- database/augmentStats.go
+++ database/augmentStats.go
@@ -11,11 +11,11 @@ func InsertOrUpdateAugmentStats(tx *sql.Tx, augmentStats *types.AugmentStatsArr)
 		// Use INSERT ... ON CONFLICT to handle conflicts and update if necessary
 		_, err := tx.Exec(`
 			INSERT INTO tft_augment_stats (match_date, game_version, augment_id, pick, accumulated_placement, frequency)
-			VALUES ($1, $2, $3, $4, $6, $7)
-			ON CONFLICT (match_date, game_version, augment_id, pick, tier)
+			VALUES ($1, $2, $3, $4, $5, $6)
+			ON CONFLICT (match_date, game_version, augment_id, pick)
 			DO UPDATE SET
-				accumulated_placement = tft_augment_stats.accumulated_placement + $6,
-				frequency = tft_augment_stats.frequency + $7
+				accumulated_placement = tft_augment_stats.accumulated_placement + $5,
+				frequency = tft_augment_stats.frequency + $6
 		`,
 			stat.GameDate, stat.GameVersion, stat.AugmentId, stat.Pick, stat.AccumulatedPlacement, stat.Frequency)
 		if err != nil {
